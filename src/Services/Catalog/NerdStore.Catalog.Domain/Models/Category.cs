53d1f2886d39407dd83c4a0595712e01df70536d
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2023-11-08 09:14:52 -0300

    feat: Implementação do AssertionConcern para validação de entidades

--- src/Services/Catalog/NerdStore.Catalog.Domain/Models/Category.cs
+++ src/Services/Catalog/NerdStore.Catalog.Domain/Models/Category.cs
@@ -11,7 +11,15 @@ public class Category : Entity
     {
         Name = name;
         Code = code;
+
+        Validate();
     }
 
     public override string ToString() => $"{Name} - {Code}";
+
+    public void Validate()
+    {
+        AssertionConcern.ValidateIfEmpty(Name, "O campo Nome da categoria não pode estar vazio");
+        AssertionConcern.ValidateIfEqual(Code, 0, "O campo Codigo não pode ser 0");
+    }
 }
