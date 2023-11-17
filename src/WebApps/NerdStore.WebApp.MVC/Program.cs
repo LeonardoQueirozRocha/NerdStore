d32d80c00ac770997e29b662297390543cae5d3a
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2023-11-17 00:18:14 -0300

    feat: Inicio da implementação da camada de apresentaçào

---
+++ src/WebApps/NerdStore.WebApp.MVC/Program.cs
@@ -0,0 +1,12 @@
+using NerdStore.WebApp.MVC.Configurations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddIdentityConfiguration(builder.Configuration);
+builder.Services.AddMvcConfiguration(builder.Configuration);
+builder.Services.AddDependencies();
+
+var app = builder.Build();
+
+app.UseMvcConfiguration(app.Environment);
+app.Run();
