6d34c4df6dcad6d831da0989f9ee869220d12824
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2023-11-23 08:57:09 -0300

    feat: Inicio da implementação do CQRS

--- src/WebApps/NerdStore.WebApp.MVC/Configurations/DependencyInjectionConfiguration.cs
+++ src/WebApps/NerdStore.WebApp.MVC/Configurations/DependencyInjectionConfiguration.cs
@@ -8,6 +8,8 @@ using NerdStore.Catalog.Domain.Interfaces.Repositories;
 using NerdStore.Catalog.Domain.Interfaces.Services;
 using NerdStore.Catalog.Domain.Services;
 using NerdStore.Core.Bus;
+using NerdStore.Sales.Application.Commands;
+using NerdStore.Sales.Application.Commands.Handlers;
 
 namespace NerdStore.WebApp.MVC.Configurations;
 
@@ -15,11 +17,17 @@ public static class DependencyInjectionConfiguration
 {
     public static void AddDependencies(this IServiceCollection services)
     {
+        // MediatR
         services.AddScoped<IMediatrHandler, MediatrHandler>();
+
+        // Catalog
         services.AddScoped<IProductRepository, ProductRepository>();
         services.AddScoped<IProductAppService, ProductAppService>();
         services.AddScoped<IStockService, StockService>();
         services.AddScoped<CatalogContext>();
         services.AddScoped<INotificationHandler<ProductBelowStockEvent>, ProductEventHandler>();
+
+        // Sales
+        services.AddScoped<IRequestHandler<AddOrderItemCommand, bool>, OrderCommandHandler>();
     }
 }
